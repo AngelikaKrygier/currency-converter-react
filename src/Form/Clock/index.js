70f39827e4716d47711ddf6cf969482c34983d68
Angelika Krygier <angelika.krygier96@gmail.com>
2023-03-13 11:59:22 +0100

    added useCurrentDate

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,33 +1,24 @@
-import { useState, useEffect } from "react";
 import { StyledClock } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
+
+
+const currentDateView = (currentDate) => currentDate.toLocaleString("pl-PL", {
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+});
 
 const Clock = () => {
-    const [currentDate, setCurrentDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
-
-    const currentDateView = currentDate.toLocaleString("pl-PL", {
-        weekday: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    })
+    const date = useCurrentDate();
 
     return (
         <StyledClock>
             Dzisiaj jest {" "}
-            {currentDateView}
+            {currentDateView(date)}
         </StyledClock>
     )
 };
