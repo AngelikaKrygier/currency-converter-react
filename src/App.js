40ccb00360b1a6b6b9b1e2361ecb3315eaccf83d
Angelika Krygier <angelika.krygier96@gmail.com>
2023-03-14 20:18:51 +0100

    moved "calculateResult" function to Form component and added (in Form component) Loading, Message, Error components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import Form from "./Form";
-import React, { useState } from "react";
-import { currencies } from './currencies.js'
 import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
 
-  const [result, setResult] = useState(null)
-
-  const calculateResult = (amount, currency) => {
-    const rate = currencies.find(({ short }) => short === currency).rate
-
-    setResult({
-      targetAmount: amount / rate,
-      currency
-    })
-  };
-
   return (
     <div className="container">
-      <GlobalStyle/>
-      <Form
-        result={result}
-        calculateResult={calculateResult} />
+      <GlobalStyle />
+      <Form />
     </div>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
-import { currencies } from '../currencies.js'
 import Result from "./Result";
 import Clock from "./Clock";
-import { Fieldset, Legend, LabelText, Input, Select, Button } from "./styled";
+import { Fieldset, Legend, LabelText, Input, Select, Button, Loading, Error, Message } from "./styled";
+import { useRatesDate } from "./useRatesDate.js";
 
-const Form = ({ result, calculateResult }) => {
+const Form = () => {
+
     const [amount, setAmount] = useState("");
-    const [currency, setCurrency] = useState(currencies[0].short);
+    const [currency, setCurrency] = useState("EUR");
+    const [result, setResult] = useState("")
+    const ratesData = useRatesDate()
+
+    const calculateResult = (amount, currency) => {
+        const rate = ratesData.rates[currency];
+
+        setResult({
+            targetAmount: amount * rate,
+            currency,
+        })
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -16,49 +28,64 @@ const Form = ({ result, calculateResult }) => {
     return (
         < form
             onSubmit={onFormSubmit}>
-            <Fieldset>
-                <Clock />
-                <Legend>KALKULATOR WALUT</Legend>
-                <p>
-                    <label> <LabelText> kwota w zł*: </LabelText>
-                        <Input
-                            type="number"
-                            name="amount"
-                            min="1"
-                            step="any"
-                            required={true}
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                        />
-                    </label>
-                </p>
-                <p>
-                    <label> <LabelText>waluta</LabelText>
-                        <Select
-                            name="currency"
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                        >
-                            {currencies.map((currency =>
-                                <option
-                                    key={currency.short}
-                                    value={currency.short}
+            {ratesData.status === "loading" ? (
+                <Loading>
+                    Właśnie ładują się kursy walut z Europejskiego Banku Centralnego! 🙂
+                </Loading>
+            ) : ratesData.status === "error" ? (
+                <Error>
+                    Coś poszło nie tak 😞 <br />
+                    Sprawdź czy masz połączenie z internetem🙂<br />
+                    Jeśli tak, daj nam chwilkę na naprawę błędu, jeśli możesz - wróć do nas póżniej!🙂
+                </Error>
+            ) : (
+                <>
+                    <Fieldset>
+                        <Clock />
+                        <Legend>KALKULATOR WALUT</Legend>
+                        <p>
+                            <label> <LabelText> kwota w zł*: </LabelText>
+                                <Input
+                                    type="number"
+                                    name="amount"
+                                    min="1"
+                                    step="any"
+                                    required={true}
+                                    value={amount}
+                                    onChange={({ target }) => setAmount(target.value)}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <label> <LabelText>waluta</LabelText>
+                                <Select
+                                    name="currency"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
                                 >
-                                    {currency.short}
-                                </option>
-                            ))}
-                        </Select>
-                    </label>
-                </p>
-                <p>
-                    <Result
-                        result={result}
-                    />
-                </p>
-            </Fieldset>
-            <Button>
-                Przelicz!
-            </Button>
+                                    {Object.keys(ratesData.rates).map(((currency) =>
+                                        <option
+                                            key={currency}
+                                            value={currency}
+                                        >
+                                            {currency}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </label>
+                        </p>
+                        <p>
+                            <Result
+                                result={result}
+                            />
+                        </p>
+                        <Message>Kursy walut pobierane są z Europejskiego Banku Centralnego.<br /> Aktualne na dzień:<br /> {ratesData.date}</Message>
+                    </Fieldset>
+                    <Button>
+                        Przelicz!
+                    </Button>
+                </>
+            )}
         </form >
     );
 };
