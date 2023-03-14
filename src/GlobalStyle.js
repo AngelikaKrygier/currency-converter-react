606e2d5dc0b34188c746c297c98de61c2aaf22b7
Angelika Krygier <angelika.krygier96@gmail.com>
2023-03-14 09:24:51 +0100

    added GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-image: url("images/background.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  .container {
+    font-size: 20px;
+    line-height: 2;
+    text-align: left;
+    margin: 20px auto;
+    max-width: 700px;
+    padding: 20px;
+  }
+
+  .container__paragraph {
+    margin: 0px;
+    font-style: italic;
+    font-size: 15px;
+  }
+`;
