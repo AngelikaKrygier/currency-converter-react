0efd0cfd3fd3cd0fb3a649b5301b81d41a1acb2d
Angelika Krygier <angelika.krygier96@gmail.com>
2023-03-19 13:49:50 +0100

    added const API_URL

--- src/Form/useRatesDate.js
+++ src/Form/useRatesDate.js
@@ -6,16 +6,19 @@ export const useRatesDate = () => {
         status: "loading",
     })
 
+    const API_URL = "https://api.exchangerate.host/latest?base=PLN"
+
     useEffect(() => {
         const getRates = async () => {
             try {
-                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const response = await axios.get(API_URL);
                 setRateDate({
                     status: "success",
                     rates: response.data.rates,
                     date: response.data.date
                 });
             } catch {
+
                 setRateDate({
                     status: "error",
                 });
